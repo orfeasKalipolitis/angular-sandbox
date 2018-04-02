07277420c842c54517e1eed4cecaef1b0b5a3cc5
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2018-04-02 16:52:21 +0300

    Router and legwork

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserPageCreatorComponent } from './user-page-creator/user-page-creator.component';
+
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  {
+    component: AboutComponent,
+    path: 'about'
+  },
+  {
+    component: UserPageCreatorComponent,
+    path: 'userPageCreator'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserPageCreatorComponent } from './user-page-creator/user-page-creator.component';
 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserPageCreatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
